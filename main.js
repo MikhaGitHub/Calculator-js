faa2cb9aed0ecc879d40c0a252b0908bf3ba52d5
MikhaGitHub <https://github.com/MikhaGitHub/Parcing-data-from-football-matches>
2024-12-25 19:25:44 +0300

    dried the code

--- main.js
+++ main.js
@@ -1,44 +1,41 @@
-function addition() {
-    const number_1 = document.getElementById("form1");
-    const number_2 = document.getElementById("form2");
-    value_1 = parseInt(number_1.value);
-    value_2 = parseInt(number_2.value);
-
-
-    result_form_id = document.getElementById("result");
-    result_form_id.innerHTML = value_1 + value_2;
-};
-
-
-function subtraction() {
-    const number_1 = document.getElementById("form1");
-    const number_2 = document.getElementById("form2");
-    value_1 = parseInt(number_1.value);
-    value_2 = parseInt(number_2.value);
-
-
-    result_form_id = document.getElementById("result");
-    result_form_id.innerHTML = value_1 - value_2;
-};
-
-function multiplication() {
-    const number_1 = document.getElementById("form1");
-    const number_2 = document.getElementById("form2");
-    value_1 = parseInt(number_1.value);
-    value_2 = parseInt(number_2.value);
-
-
-    result_form_id = document.getElementById("result");
-    result_form_id.innerHTML = value_1 * value_2;
-};
-
-function division() {
-    const number_1 = document.getElementById("form1");
-    const number_2 = document.getElementById("form2");
-    value_1 = parseInt(number_1.value);
-    value_2 = parseInt(number_2.value);
-
-
-    result_form_id = document.getElementById("result");
-    result_form_id.innerHTML = value_1 / value_2;
-};
+function calculate(operation) {
+    const num1 = parseInt(document.getElementById("form1").value);
+    const num2 = parseInt(document.getElementById("form2").value);
+    const resultElement = document.getElementById("result");
+  
+    let result;
+    switch (operation) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num1 / num2;
+        break;
+      default:
+        result = "Invalid operation";
+    }
+  
+    resultElement.textContent = result; // Лучше использовать textContent вместо innerHTML для безопасности и производительности
+  }
+  
+  function addition() {
+    calculate('+');
+  }
+  
+  function subtraction() {
+    calculate('-');
+  }
+  
+  function multiplication() {
+    calculate('*');
+  }
+  
+  function division() {
+    calculate('/');
+  }  
